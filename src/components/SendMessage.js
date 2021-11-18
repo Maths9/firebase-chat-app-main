63e2123e5d95d90dd9140f17a1bd8842279206ee
Maths9 <mbl@discente.ifpe.edu.br>
2021-11-18 15:53:28 -0300

    18/11

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -23,7 +23,7 @@ function SendMessage({ scroll }) {
         <div>
             <form onSubmit={sendMessage}>
                 <div className="sendMsg">
-                    <Input style={{ width: '78%', fontSize: '15px', fontWeight: '550', marginLeft: '5px', marginBottom: '-3px' }} placeholder='Message...' type="text" value={msg} onChange={e => setMsg(e.target.value)} />
+                    <Input style={{ width: '78%', fontSize: '15px', fontWeight: '550', marginLeft: '5px', marginBottom: '-3px' }} placeholder='Mensagem' type="text" value={msg} onChange={e => setMsg(e.target.value)} />
                     <Button style={{ width: '18%', fontSize: '15px', fontWeight: '550', margin: '4px 5% -13px 5%', maxWidth: '200px'}} type="submit">Enviar</Button>
                 </div>
             </form>
