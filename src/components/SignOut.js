cad7a343f2a4f9689308e1bbb7331115134c5874
Maths9 <mbl@discente.ifpe.edu.br>
2021-11-18 23:49:43 -0300

    18/11 - 23:49

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -4,10 +4,10 @@ import { Button } from '@material-ui/core'
 
 function SignOut() {
     return (
-        <div style={{
-            display: 'flex', justifyContent: 'center', position: 'fixed', width: '100%', backgroundColor: '#FAFAFA', top: 0, borderBottom: 'solid 1px lightgray', zIndex: '10'
+        <div class="header" style={{
+            display: 'flex', justifyContent: 'flex-end', position: 'fixed', width: '100%', backgroundColor: '#A6BF4B', top: 0, zIndex: '10'
         }}>
-            <Button style={{ padding: '20px', fontSize: '15px', borderRadius: '0', fontWeight: '600' }} onClick={() => auth.signOut()}>Sign Out</Button>
+            <Button style={{ padding: '20px', fontSize: '20px', borderRadius: '0', fontWeight: '600' }} onClick={() => auth.signOut()}>SAIR</Button>
         </div>
     )
 }
