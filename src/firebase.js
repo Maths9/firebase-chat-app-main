4e5f1e084d527ea38986c769495234f6cede8bfb
Maths9 <mbl@discente.ifpe.edu.br>
2021-12-06 19:35:22 -0300

    06/12

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDvWrV8p5cm5W6mL0m9GFPTA2N3d0yVrt8",
-    authDomain: "boletim-68c17.firebaseapp.com",
-    databaseURL: "https://boletim-68c17-default-rtdb.firebaseio.com",
-    projectId: "boletim-68c17",
-    storageBucket: "boletim-68c17.appspot.com",
-    messagingSenderId: "260092741979",
-    appId: "1:260092741979:web:1b6bc9195bda66ab8d727e"
+   apiKey: "AIzaSyBAS9H6QjwgDJF_DRAQWmWZx3ngbZyaHnE",
+  authDomain: "app-ema-8dd6a.firebaseapp.com",
+  databaseURL: "https://app-ema-8dd6a-default-rtdb.firebaseio.com",
+  projectId: "app-ema-8dd6a",
+  storageBucket: "app-ema-8dd6a.appspot.com",
+  messagingSenderId: "448239956919",
+  appId: "1:448239956919:web:ef425f78cf2f2212576e32"
 })
 
 const db = firebaseApp.firestore()
